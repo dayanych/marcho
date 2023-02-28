6d64b2a275d6e2c2ae0880d25f9df86561ea8ff4
Dayana <dayana.shakenova20@gmail.com>
2023-02-28 17:57:35 +0600

    feat: product

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,17 @@ $(function(){
     dots: true,
     arrows: false,
     fade: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-  })  
+    autoplay: true,
+    autoplaySpeed: 2000,
+  }) 
+  $('.star').rateYo({
+    starWidth: '17px',
+    normalFill: '#ccccce',
+    ratedFill: '#FFC35B',
+    readOnly: 'true'
+  })
 })
 Fancybox.bind("[data-fancybox]", {
   infinite: false,
 });
+
