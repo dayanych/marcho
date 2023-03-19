a49b8f7d9e950ded9e100ded6f9bcef38aee167f
Dayana <dayana.shakenova20@gmail.com>
2023-03-19 20:32:28 +0600

    feat: product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $(function(){
-
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
   $('.shop-content__filter-btn').on('click', function() {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
@@ -13,7 +33,7 @@ $(function(){
     $('.product-item').removeClass('product-item--list');
   });
 
-  $('.select-style').styler({});
+  $('.select-style, .product-one__num').styler({});
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
@@ -40,7 +60,8 @@ $(function(){
     starWidth: '17px',
     normalFill: '#ccccce',
     ratedFill: '#FFC35B',
-    readOnly: 'true'
+    readOnly: 'true',
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
   });
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
@@ -82,6 +103,7 @@ $(function(){
   }
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock(deadline);
+  
 })
 Fancybox.bind("[data-fancybox]", {
   infinite: false,
